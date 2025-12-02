92ee708b802678bf8442cd5a5a4b43fb3301ac1d
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-12-02 11:51:39 +0100

    Utils.py actualizado

---
+++ planify_server/api/utils.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from .models import AuthToken
+
+def require_token(view_func):
+    def wrapped(request, *args, **kwargs):
+        auth = request.headers.get('Authorization') or request.META.get('HTTP_AUTHORIZATION')
+        if not auth:
+            return JsonResponse({'error': 'Missing Authorization header'}, status=401)
+        if auth.startswith('Bearer '):
+            token = auth.split(' ',1)[1]
+        else:
+            token = auth
+        try:
+            at = AuthToken.objects.get(key=token)
+            request.current_user = at.user
+        except AuthToken.DoesNotExist:
+            return JsonResponse({'error': 'Invalid token'}, status=401)
+        return view_func(request, *args, **kwargs)
+    wrapped.__name__ = view_func.__name__
+    return wrapped
