d022791ee25674fe0bf3de42222c0b1617055e7f
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-12-02 11:40:55 +0100

    Admin creado

--- planify_server/api/admin.py
+++ planify_server/api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, AuthToken, Project, Task
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','created_at')
+    search_fields = ('name','email')
+
+@admin.register(AuthToken)
+class AuthTokenAdmin(admin.ModelAdmin):
+    list_display = ('user','key','created_at')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id','name','created_at')
+    search_fields = ('name',)
+    filter_horizontal = ('members',)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id','title','project','state','assigned_to','created_at')
+    list_filter = ('state','project')
+
+
+
+
