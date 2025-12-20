31b97429b42c857822044e00b4490018d9131292
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-12-20 12:45:45 +0100

    Navegacion entre Welcome a Login y Register

--- app/src/main/java/com/example/planify/LoginActivity.java
+++ app/src/main/java/com/example/planify/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.example.planify;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/planify/MainActivity.java
+++ app/src/main/java/com/example/planify/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.planify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        View btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+        btnRegister.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        });
     }
 }
