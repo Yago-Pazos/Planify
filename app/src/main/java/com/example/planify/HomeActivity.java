fe774cdacae76776cd79cfe3a8447361e3b25c52
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-12-20 13:08:58 +0100

    Navegacion desde Home

--- app/src/main/java/com/example/planify/HomeActivity.java
+++ app/src/main/java/com/example/planify/HomeActivity.java
@@ -1,6 +1,11 @@
 package com.example.planify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -9,5 +14,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View cardProject = findViewById(R.id.cardProject);
+        Button btnNewProject = findViewById(R.id.btnNewProject);
+        ImageView btnLogout = findViewById(R.id.btnLogout);
+
+        cardProject.setOnClickListener(v ->
+                startActivity(new Intent(this, ProjectDetailActivity.class))
+        );
+
+        btnNewProject.setOnClickListener(v -> {
+            // luego abriremos el dialog_create_project
+        });
+
+        btnLogout.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
     }
 }
