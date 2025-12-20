0a39308ce1dea777e41daea8663d746413598d34
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2025-12-20 12:58:40 +0100

    Navegacion entre Login y Register

--- app/src/main/java/com/example/planify/LoginActivity.java
+++ app/src/main/java/com/example/planify/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.planify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Botón Entrar
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            finish();
+        });
+
+        // TEXTO "Crear cuenta"
+        TextView tvGoToRegister = findViewById(R.id.tvGoToRegister);
+        tvGoToRegister.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
     }
 }
--- app/src/main/java/com/example/planify/RegisterActivity.java
+++ app/src/main/java/com/example/planify/RegisterActivity.java
@@ -1,13 +1,32 @@
 package com.example.planify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+            finish();
+        });
+        TextView tvGoToLogin = findViewById(R.id.tvGoToLogin);
+
+        tvGoToLogin.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+
     }
 }
